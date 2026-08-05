0f23d7179512ff69aca98aab73a6aacdb9c06f8a
Mathias SEGUIN-CADICHE <mathias.seguincadiche@gmail.com>
2026-08-05 02:19:41 -0400

    security: imposer IMDSv2 et le chiffrement EC2

--- terraform/exercice-1/main.tf
+++ terraform/exercice-1/main.tf
@@ -139,9 +139,22 @@ resource "aws_instance" "web" {
   ami                         = coalesce(var.ami_id, data.aws_ami.ubuntu.id)
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public[0].id
+  associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.web.id]
   key_name                    = aws_key_pair.p5.key_name
   user_data_replace_on_change = true
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  root_block_device {
+    encrypted             = true
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
 
   user_data = <<-EOF_USER_DATA
     #!/usr/bin/env bash
